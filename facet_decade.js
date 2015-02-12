4b592124e49042c63d2d7821dd1a0f0f4f02cb87
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2015-02-12 15:25:23 -0800

    javascript version

---
+++ facet_decade.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+var facetDecade = function(string) {
+  var year = new Date().getFullYear();
+  var re = /(\d+)/g;
+  var matches = string.match(re);
+  matches = matches.filter(function(n) {
+    return n > 1000 && n <= year;
+  });
+  var max = Math.max.apply(null, matches);
+  var min = Math.min.apply(null, matches);
+  min = Math.floor(min / 10) * 10;
+  var decades = [];
+  for (x = min; x <= max; x = x + 10) {
+    decades.push(x + 's');
+  }
+  return decades;
+}
+
+if (process.argv.length <= 2) {
+  console.log('usage: facet_decade.js: "date string" ["date string" ...]');
+}
+
+for (var i = 2; i < process.argv.length; i++){
+  console.dir(
+    facetDecade(process.argv[i])
+  );
+}
+
+module.exports = facetDecade;
+
+/*
+Copyright © 2015, Regents of the University of California
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+ * Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in
+   the documentation and/or other materials provided with the
+   distribution.
+
+ * Neither the name of the University of California nor the names
+   of its contributors may be used to endorse or promote products
+   derived from this software without specific prior written
+   permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
