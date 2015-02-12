613c9e76938fb217820ec1103c99d4bc12811a8e
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2015-02-12 00:01:52 -0800

    assume future dates are bogus

--- org/cdlib/dsc/util/FacetDecade.java
+++ org/cdlib/dsc/util/FacetDecade.java
@@ -15,15 +15,6 @@
  *		
  * Author:	Michael A. Russell
  *
- * Revision History:
- *		2008/5/7 - MAR - Initial writing
- *		2008/5/7 - bct - added package, tweaked output
- *		2008/9/15 - MAR - It looks like Saxon is choking on
- *			when the output is the zero-length string.
- *			Always return valid XML.
- *		2009/3/4 - MAR - Ignore year "0000".  Use the current
- *			year as the absolute maximum.  Ignore any year
- *			less than 1000.
  * ------------------------------------
  */
 
@@ -86,10 +77,10 @@ public class FacetDecade {
 	    /* If the year is less than 1000, ignore it.  */
 	    if (iyear < 1000) continue;
 
-	    /* If the year is greater than the current year, use the
-	     * current year.
+	    /* If the year is greater than the current year, 
+             * ignore it.
 	     */
-	    if (iyear > now.get(Calendar.YEAR)) iyear = now.get(Calendar.YEAR);
+	    if (iyear > now.get(Calendar.YEAR)) continue;
 
 	    if (iyear < min) min = iyear;
 	    if (iyear > max) max = iyear;
