9a068f15698bb59a61d6c9915fd3a0a1a8cadfcc
BRIAN TINGLE <brian.tingle.cdlib.org@gmail.com>
2015-02-16 12:09:46 -0800

    perl version, add licnese, don't use say/Dumper

--- facet_decade.pl
+++ facet_decade.pl
@@ -1,15 +1,12 @@
 #!/usr/bin/env perl
 
 use Time::Piece;
-use Data::Dumper;
 use List::MoreUtils qw( minmax );
 use POSIX qw( floor );
 
 use strict;
 use warnings;
 use utf8;
-use 5.010;
-use feature qw( say );
 use JSON qw( to_json );
 
 sub facet_decade {
@@ -33,7 +30,7 @@ sub facet_decade {
 sub run {
   my( $class, @args ) = @_;
   if (!@args) {
-    say('usage: facet_decade.pl: "date string" ["date string" ...]');
+    print('usage: facet_decade.pl: "date string" ["date string" ...]\n');
     exit();
     }
   for my $string (@args) {
@@ -45,3 +42,30 @@ sub run {
 __PACKAGE__->run( @ARGV ) unless caller;
 
 1;
+
+=begin license
+Copyright © 2015, Regents of the University of California
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+- Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+- Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+- Neither the name of the University of California nor the names of its
+  contributors may be used to endorse or promote products derived from this
+  software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+=end license
+=cut
